aa1f1a4b92162c8ada6b3a707a1e12328ad89135
jlrt37 <joshua.l.ragon@gmail.com>
2019-01-26 10:58:02 -0600

    server.js update main

--- my-app/server.js
+++ my-app/server.js
@@ -11,10 +11,74 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(logger("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/SuperScriber";
+mongoose.connect("mongodb://localhost/populate", { useNewUrlParser: true });
 
-mongoose.connect(MONGODB_URI);
+var db = mongojs(MONGODB_URI);
 
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+app.post("/submit", function(req, res) {
+    db.users.create(req.body)
+      .then(function(dbUsers) {
+        return db.Users.findOneAndUpdate({}, { $push: { users: dbUsers._id } }, { new: true });
+      })
+      .then(function(dbUsers) {
+        res.json(dbUsers);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+app.post("/submit", function(req, res) {
+    db.Items.create(req.body)
+      .then(function(dbItems) {
+        return db.Items.findOneAndUpdate({}, { $push: { items: dbItems._id } }, { new: true });
+      })
+      .then(function(dbItems) {
+        res.json(dbItems);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+
+app.get("/index", function(req, res) {
+    db.Items.find({})
+    .then(function(dbItems) {
+      res.json(dbItems);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+app.get("/index", function(req, res) {
+    db.Users.find({})
+    .then(function(dbUsers) {
+      res.json(dbUsers);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});  
+
+app.get("/populated", function(req, res) {
+    db.Items.find({})
+      .populate("items")
+      .then(function(dbItems) {
+        res.json(dbItems);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
