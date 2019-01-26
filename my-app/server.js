aa02593267264e1e78556d621ae95b4d2ea15b3f
jlrt37 <joshua.l.ragon@gmail.com>
2019-01-26 12:09:24 -0600

    added controllers

--- my-app/server.js
+++ my-app/server.js
@@ -3,6 +3,8 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var path = require("path");
+var axios = require("axios");
+var react = require("react")
 
 var db = require("./models");
 
@@ -11,74 +13,102 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/populate", { useNewUrlParser: true });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+requirePath({
+  path: path.join(__dirname, ''),
+
+app.use(axios.json());
+app.use(axios.urlencoded({ extended: true }));
+
+app.use(react.json());
+app.use(react.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost/populate", {
+  useNewUrlParser: true
+});
 
 var db = mongojs(MONGODB_URI);
 
-db.on("error", function(error) {
+db.on("error", function (error) {
   console.log("Database Error:", error);
 });
-app.post("/submit", function(req, res) {
-    db.users.create(req.body)
-      .then(function(dbUsers) {
-        return db.Users.findOneAndUpdate({}, { $push: { users: dbUsers._id } }, { new: true });
-      })
-      .then(function(dbUsers) {
-        res.json(dbUsers);
-      })
-      .catch(function(err) {
-        res.json(err);
+app.post("/submit", function (req, res) {
+  db.users.create(req.body)
+    .then(function (dbUsers) {
+      return db.Users.findOneAndUpdate({}, {
+        $push: {
+          users: dbUsers._id
+        }
+      }, {
+        new: true
       });
-  });
-
-app.post("/submit", function(req, res) {
-    db.Items.create(req.body)
-      .then(function(dbItems) {
-        return db.Items.findOneAndUpdate({}, { $push: { items: dbItems._id } }, { new: true });
-      })
-      .then(function(dbItems) {
-        res.json(dbItems);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
-
-
-app.get("/index", function(req, res) {
-    db.Items.find({})
-    .then(function(dbItems) {
-      res.json(dbItems);
     })
-    .catch(function(err) {
+    .then(function (dbUsers) {
+      res.json(dbUsers);
+    })
+    .catch(function (err) {
       res.json(err);
     });
 });
-app.get("/index", function(req, res) {
-    db.Users.find({})
-    .then(function(dbUsers) {
-      res.json(dbUsers);
+
+app.post("/submit", function (req, res) {
+  db.Items.create(req.body)
+    .then(function (dbItems) {
+      return db.Items.findOneAndUpdate({}, {
+        $push: {
+          items: dbItems._id
+        }
+      }, {
+        new: true
+      });
     })
-    .catch(function(err) {
+    .then(function (dbItems) {
+      res.json(dbItems);
+    })
+    .catch(function (err) {
       res.json(err);
     });
-});  
+});
 
-app.get("/populated", function(req, res) {
-    db.Items.find({})
-      .populate("items")
-      .then(function(dbItems) {
-        res.json(dbItems);
-      })
-      .catch(function(err) {
-        res.json(err);
-      });
-  });
 
-app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
+app.get("/index", function (req, res) {
+  db.Items.find({})
+    .then(function (dbItems) {
+      res.json(dbItems);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+app.get("/index", function (req, res) {
+  db.Users.find({})
+    .then(function (dbUsers) {
+      res.json(dbUsers);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.get("/populated", function (req, res) {
+  db.Items.find({})
+    .populate("items")
+    .then(function (dbItems) {
+      res.json(dbItems);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
